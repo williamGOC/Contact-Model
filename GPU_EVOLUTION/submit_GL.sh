756d3d9f60c27ad94fbbc3836980c5da80ebb582
William Carreras <37045478+williamGOC@users.noreply.github.com>
2019-10-21 22:40:53 -0300

    Delete submit_GL.sh

--- GPU_EVOLUTION/submit_GL.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-
-nvcc CP_GPU.cu -arch=sm_20 -o CP_GPU.out -lglut -lGLU -lGL -lcufft -DIM
-./CP_GPU.out
